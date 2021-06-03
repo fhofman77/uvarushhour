316872e4884a9036eb81b6713f50c8806f26b47e
Frank Hofman <frank-hofman77@live.nl>
2021-06-03 15:21:55 +0200

    changes

---
+++ code/algorithms/move.py
@@ -0,0 +1,32 @@
+from code.classes.objects import Car
+from ..visualisation.visualise import visualise
+import csv
+moves = []
+
+
+def move(Car, distance):
+    # if the car is moved, append to moves
+    if distance != 0:
+        moves.append(Car, distance)
+
+    # move the car
+    if Car.orientation == 'H':
+        Car.col += distance
+    if Car.orientation == 'V':
+        Car.row += distance
+
+    return Car
+
+
+vehicles = []
+with open('data/gameboards/Rushhour6x6_1.csv', newline='') as gamefile:
+    rows = csv.reader(gamefile, delimiter=',')
+    next(rows)
+    count = 0
+    for row in rows:
+        vehicle = Car(row[0], row[1], row[2], row[3], row[4])
+        vehicles.append(vehicle)
+
+print(vehicles[0])
+move(vehicles[0])
+print(moves)
