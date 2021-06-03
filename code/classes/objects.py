898bc3ff4cefda6beed4789db694d8c16356749b
Frank Hofman <frank-hofman77@live.nl>
2021-06-03 10:15:21 +0200

    added classes

---
+++ code/classes/objects.py
@@ -0,0 +1,12 @@
+class Board(self, size):
+    # columns run vertical (like a y axes)
+    self.col = size
+    # rows run horizontal (like a x axes)
+    self.row = size
+
+class Car(self, car, orientation, col, row, length):
+    self.car = car
+    self.orientation = orientation
+    self.col = col
+    self.row = row
+    self.length = length
