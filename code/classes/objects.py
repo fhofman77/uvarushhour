8d783dc5bfeea8d4ab19dc92a85bae6209970ffa
Frank Hofman <frank-hofman77@live.nl>
2021-06-03 10:21:52 +0200

    done making classes now working on adding load in function

--- code/classes/objects.py
+++ code/classes/objects.py
@@ -3,8 +3,10 @@ class Board(self, size):
     self.col = size
     # rows run horizontal (like a x axes)
     self.row = size
+    self.exit = pass
 
 class Car(self, car, orientation, col, row, length):
+    # in the data the last car is the car that needs to escape
     self.car = car
     self.orientation = orientation
     self.col = col
