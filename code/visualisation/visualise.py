e71990f263e282ade91751950426553d68883b19
Frank Hofman <frank-hofman77@live.nl>
2021-06-03 11:40:57 +0200

    could not get class Car imported

--- code/visualisation/visualise.py
+++ code/visualisation/visualise.py
@@ -1,3 +1,12 @@
 import csv
+from .code import Car
 
+
+with open('/Users/frank/Documents/UVA 2020:2021/Programmeren/programeer theorie/uvarushhour/data/gameboards/Rushhour6x6_1.csv', newline='') as gamefile:
+    rows = csv.reader(gamefile, delimiter=',')
+    next(rows)
+    for row in rows:
+        Car(row[0], row[1], row[2], row[3], row[4])
+        print(Car.id)
+    
 
