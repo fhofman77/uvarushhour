0b9474124c195269225d15cbbd237dad109d0647
Merrin Maas <merrin.maas@student.uva.nl>
2021-06-03 14:24:35 +0200

    new move function draft

---
+++ code/algorithms/move.py
@@ -0,0 +1,11 @@
+moves = []
+
+def move(Car, distance):
+    # move the car
+    
+
+    # if the car is moved, append to moves
+    if distance != 0:
+        moves.append(Car, distance)
+
+    # return Car
