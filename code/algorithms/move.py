2603f9918e9225da3bfc08763cc3e860b846aeb9
Merrin Maas <merrin.maas@student.uva.nl>
2021-06-03 14:40:54 +0200

    probeer move te testen

--- code/algorithms/move.py
+++ code/algorithms/move.py
@@ -12,3 +12,16 @@ def move(Car, distance):
         Car.row += distance
 
     return Car
+
+vehicles = []
+with open('code/data/gameboards/Rushhour6x6_1.csv', newline='') as gamefile:
+    rows = csv.reader(gamefile, delimiter=',')
+    next(rows)
+    count = 0
+    for row in rows:
+        vehicle = Car(row[0], row[1], row[2], row[3], row[4])
+        vehicles.append(vehicle)
+
+print(vehicles[0])
+move(vehicles[0], 1)
+print(moves)
