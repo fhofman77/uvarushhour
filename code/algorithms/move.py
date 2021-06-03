cd55c28eab0dd086c65651f4b9a4902af8abe2ed
Merrin Maas <merrin.maas@student.uva.nl>
2021-06-03 14:30:18 +0200

    car is moved and object changed

--- code/algorithms/move.py
+++ code/algorithms/move.py
@@ -1,11 +1,14 @@
 moves = []
 
 def move(Car, distance):
-    # move the car
-    
-
     # if the car is moved, append to moves
     if distance != 0:
         moves.append(Car, distance)
 
-    # return Car
+    # move the car
+    if Car.orientation == 'H':
+        Car.col += distance
+    if Car.orientation == 'V':
+        Car.row += distance
+
+    return Car
