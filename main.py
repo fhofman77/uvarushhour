0fa7b80e354559467e63c0b2a85754a000694714
Felix-Blom <70511386+Felix-Blom@users.noreply.github.com>
2021-06-07 22:19:14 +0200

    Revert "Revert "made some changes im going to revert""
    This reverts commit 07d790f74a4eb693317c87d09c2abf4afe4c7ae1.

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from code.classes.objects import moves
-from code.visualisation.visualise import initialize_cars, get_board_size, print_board
+from code.visualisation.visualise import print_board
+from code.classes.objects import Board, get_board_size, moves
 
-inputdata = 'data/gameboards/Rushhour6x6_1.csv'
+inputdata = 'data/gameboards/Rushhour6x6_3.csv'
 # Load all the vehicles
-print(get_board_size(inputdata))
-vehicles = initialize_cars(inputdata)
 
-print_board(vehicles, get_board_size(inputdata))
+board = Board(inputdata)
+print(board.get_board_size)
+
+
+print(board.vehicles[5].car)
+
+board.vehicles[0].valid_move(-2, board)
+board.vehicles[5].valid_move(-2, board)
