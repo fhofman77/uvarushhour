0bd2671436f6fcc6ff0d7a046c2a09c8e45647a9
Frank Hofman <frank-hofman77@live.nl>
2021-06-07 10:10:41 +0200

    print function improved but not finished

--- code/visualisation/visualise.py
+++ code/visualisation/visualise.py
@@ -20,28 +20,65 @@ def initialize_cars(csv_input):
 
     # Printen misschien beter in een andere functie
     # Dit werkt nu niet voor boards met een andere size dan 6, ook klopt de logica niet
+
+    occupied_coordinates = []
+    for car in vehicles:
+        if car.orientation == 'H':
+            x = car.row
+            for i in range(int(car.length)):
+                y = car.col
+                # print(f'y = {y} and i = {i}')
+                y += i
+                car_coordinates = [x, y]
+                occupied_coordinates.append(car_coordinates)
+        else:
+            y = car.row
+            for i in range(int(car.col)):
+                x = car.col
+                # print(f'y = {y} and i = {i}')
+                x += i
+                car_coordinates = [x, y]
+                occupied_coordinates.append(car_coordinates)
+    print(occupied_coordinates)
+            
+            
+
     for row in range(6):
         for col in range(6):
-            """
-            Beter om elke voor car een occupied spaces variabele aan te maken?
-            Example: Car 1: occupied-row: [4], occupied col =[3,4,5]?
-            Print car name if the row we are in == the occupied row of a car && col == occupied col
-            Else print *
-            Deze kunnen we dan ook gebruiken om te checken of een move kan
-            """
             printed = False
-            for car in range(len(vehicles)):
-                if col + 1 == vehicles[car].col and row + 1 == vehicles[car].row:
-                    print(f'{vehicles[car].car} ', end='')
+            for i in occupied_coordinates:
+                if i[0] != row and i[1] != col and not printed:
+                    print('# ', end='')
                     printed = True
-                    if vehicles[car].orientation == 'H':
-                        print(f'{vehicles[car].car} ', end='')
-                        col += 1
-                        if vehicles[car].length == 3:
-                            print(f'{vehicles[car].car} ', end='')
-                            col += 1
-            if not printed:
-                print('# ', end='')
-        print()
+                elif not printed:
+                    print('* ', end='')
+                    printed = True
+        print('', end='\n')
 
-        return vehicles
+
+
+
+
+            # """
+            # Beter om elke voor car een occupied spaces variabele aan te maken?
+            # Example: Car 1: occupied-row: [4], occupied col =[3,4,5]?
+            # Print car name if the row we are in == the occupied row of a car && col == occupied col
+            # Else print *
+            # Deze kunnen we dan ook gebruiken om te checken of een move kan
+            # """
+        #     printed = False
+        #     for car in range(len(vehicles)):
+        #         if col + 1 == vehicles[car].col and row + 1 == vehicles[car].row:
+        #             print(f'{vehicles[car].car} ', end='')
+        #             printed = True
+        #             if vehicles[car].orientation == 'H':
+        #                 print(f'{vehicles[car].car} ', end='')
+        #                 col += 1
+        #                 if vehicles[car].length == 3:
+        #                     print(f'{vehicles[car].car} ', end='')
+        #                     col += 1
+        #     if not printed:
+        #         print('# ', end='')
+        # print()
+
+    return vehicles
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from code.visualisation.visualise import initialize_cars, get_board_size
 from code.classes.objects import moves
 import csv
 
-inputdata = 'data/gameboards/Rushhour12x12_7.csv'
+inputdata = 'data/gameboards/Rushhour6x6_1.csv'
 # Load all the vehicles
 vehicles = initialize_cars(inputdata)
 
