45c4612de16e9b0c18a7d62fbd0841dbcec007d2
Felix-Blom <70511386+Felix-Blom@users.noreply.github.com>
2021-06-07 15:04:20 +0200

    Revert "Merge pull request #1 from fhofman77/frank"
    This reverts commit ea5fb7f5d6440d487998f105b9c5187835789bde, reversing
    changes made to d0ad36898169b44a007aaa5eee41624cf11ad38c.

--- main.py
+++ main.py
@@ -5,21 +5,5 @@ inputdata = 'data/gameboards/Rushhour6x6_1.csv'
 vehicles = initialize_cars(inputdata)
 print_board(vehicles, get_board_size(inputdata))
 
-
-vehicles[0].move(1)
-vehicles[2].move(-3)
-print(f"Moves: {moves}")
-
-# Creates a CSV file from all the moves that have been added to the dict
-with open('data/output/output.csv', 'w') as csv_file:
-    writer = csv.writer(csv_file)
-    for key, value in moves.items():
-        writer.writerow([key, value])
-
-
-a = get_board_size(inputdata)
-print_board(a, vehicles)
-=======
 Car.move
 print_board()
-
