b2b94f58f37647420dcb3c30f924d73261d017f4
Frank Hofman <frank-hofman77@live.nl>
2021-06-09 12:19:12 +0200

    added printing en algo

---
+++ code/algorithms/random.py
@@ -0,0 +1,19 @@
+import random
+import copy
+# from code.classes import objects
+
+""" 
+Contains the algorithems for solving the rushhour puzzle
+"""
+
+
+
+def random_move(board):
+    # choose a random car to move
+    car = random.choice(board.vehicles)
+    
+    board.move_car(board.vehicles[-1], 1)
+    print(board.vehicles[-1].car ,board.vehicles[-1].coordinate_col)
+    number = random.randrange(int(-board.size), int(board.size))
+    board.move_car(car, number)
+    return board
