93b8a8d672fdede4dffd70df2ef2c55a9e957863
Frank Hofman <frank-hofman77@live.nl>
2021-06-14 10:39:42 +0200

    now knowing that que return a list object instead of our class object

--- code/algorithms/breath_first.py
+++ code/algorithms/breath_first.py
@@ -6,23 +6,26 @@ def breath_algorithm(board):
     def child_states(list):
         children = []
         print(f'this is board {list}')
-        # board = list[1]
+        board = list
         # print(f'this is board {board}')
+        board.print()
         for car in board.vehicles:
             for distance in range(-board.size + 1, board.size):
                 if distance != 0:
                     print(distance)
                     current_board = copy.deepcopy(board)
-                    print(current_board.move_car(car, distance))
+                    # print(current_board.move_car(car, distance))
                     if current_board.move_car(car, distance):
-                        # print('good')
+                        print('good')
                         # current_board.print()
                         route = [current_board, board, car.car, distance]
                         children.append(route)
 
             print(car.car)
             print(children)
-        print(f'childeren: {children}')
+            # return children
+            
+        # print(f'childeren: {children}')
 
         return children
 
@@ -30,29 +33,44 @@ def breath_algorithm(board):
         que = queue.Queue()
         visited = []
         que.put([[board, board]])
+        visisted_printer = []
+        count = 0
+        cycle = 0
 
         while not que.empty():
+            cycle+=1
             temp = que.get()
-            print(f'this is temp {temp}')
-            next_board = temp[0]
+            # print(f'this is temp {temp}')
+            next_board = temp
             print(f'this is next board in solver {next_board}')
             children = child_states(next_board)
             for child in children:
                 if child[0].won_game():
                     print('won')
                     return
-                # print(f'child {child[0]}')
-                # print(f'child not in {visited}')
                 if child[0].occupied_row_col not in visited:
-                    print(f'what is put {child}')
-                    que.put(child)
+                    # print(f'what is put {child}')
+                    que.put(child[0])
+                    print(f'waht is put in que {child[0].occupied_row_col}')
                     visited.append(child[0].occupied_row_col)
-                    # print(f'test: {que}')
+                    visisted_printer.append(child[0])
+                    count+=1
                 else:
-                    print('not')
-            print(que.qsize())
+                    # print('not')
+                    pass
+
         print('this is visited')
-        print(visited)
+
+        print(count)
+        
+        count = 1
+        for board in visisted_printer:
+            print(count)
+            board.print()
+            count +=1
+        
+        print(f'total cycle: {cycle}')
+
         return
 
     
--- code/classes/objects.py
+++ code/classes/objects.py
@@ -60,46 +60,29 @@ class Board():
         """Change car.car to car"""
         move_made = 0
         for item in self.occupied_row_col:
-            # print(car.orientation)
             if item[0] == car.car and car.orientation == "H":
-                # print('orientation is horizontal')
-                if self.valid_horizontal_move(car, item[2], item[2]+distance):
-                    item[2] = item[2] + distance
-                    moves[car.car] = distance
-                    print(f'this is item2 + distance: {car.car, item[2], distance}')
-                    return True
+                if self.valid_horizontal_move(car, item[2], item[2]+distance) == False:
+                    return False
             if item[0] == car.car and car.orientation == "V":
-                if self.valid_vertical_move(car, item[1], item[1]+distance):
-                    item[1] = item[1] + distance
-                    moves[car.car] = distance
-                    move_made += 1
-                    return True
 
+                if self.valid_vertical_move(car, item[1], item[1]+distance) == False:
+                    return False
 
-        # for item in self.occupied_row_col:
-        #     if item[0] == car.car and car.orientation == "H":
-                
+        for item in self.occupied_row_col:
+            if item[0] == car.car and car.orientation == "H":
+                item[2] = item[2] + distance
+                moves[car.car] = distance
+                move_made += 1
 
-        #     if item[0] == car.car and car.orientation == "V":
-        #         item[1] = item[1] + distance
-        #         moves[car.car] = distance
-        #         move_made += 1
+            if item[0] == car.car and car.orientation == "V":
+                item[1] = item[1] + distance
+                moves[car.car] = distance
+                move_made += 1
 
-        # if move_made > 0:
-        #     return True
-        # else:
-        #     return False
-    
-    def get_possible_moves(self):
-        current_board = self.occupied_row_col
-        possible_moves = []
-        for possible_move in range(-4, self.size-1):
-            print(possible_move)
-            for car in self.vehicles:
-                if current_board.move_car(car, possible_move) == True:
-                    print('car moved')
-                    possible_moves.append([car.car, possible_move])
-        print(possible_moves)
+        if move_made > 0:
+            return True
+        else:
+            return False
 
     def print(self):
         return print_board(self.occupied_row_col, self.size)
@@ -117,7 +100,7 @@ class Board():
             return True
 
     def valid_horizontal_move(self, car, startpoint, endpoint):
-        if endpoint <= 1 or endpoint > self.size:
+        if endpoint <= 0 or endpoint > self.size:
             return False
 
         """Change to car instead of car.car"""
