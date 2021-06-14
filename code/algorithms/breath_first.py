380183a133649f79be1a07303d50853b574949fd
Felix-Blom <felix.blom@live.nl>
2021-06-14 13:01:10 +0200

    trying DFS

--- code/algorithms/breath_first.py
+++ code/algorithms/breath_first.py
@@ -2,7 +2,6 @@ import queue
 import copy
 
 def breath_algorithm(board):
-
     def child_states(board):
         children = []
         for car in board.vehicles:
@@ -37,5 +36,35 @@ def breath_algorithm(board):
 
     
     solver(board)
+
+def dfs_algorithm(board):
+    def dfs_states(board):
+        children = []
+        for car in board.vehicles:
+            for distance in range(-board.size + 1, board.size):
+                if distance != 0:
+                    current_board = copy.deepcopy(board)
+                    if current_board.move_car(car, distance):
+                        children.append(board)
+
+        return children
+
+    def dfs_search(start_state):
+        queue = [[start_state]]
+        seen_states = set()
+        while queue:
+            path = queue.pop()
+            if path[-1].won_game():
+                return True
+
+            for next_state in dfs_states(path[-1]):
+                if next_state not in seen_states:
+                    seen_states.add(next_state)
+                    queue.append(path + [next_state])
+        return
+
+    dfs_search(board)
+
+    
 
 
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from code.algorithms.random import random_move
 from code.algorithms.breath_first import breath_algorithm
 from code.visualisation.visualise import print_board
 from code.classes.objects import Board, moves, Car
-
+import copy, time
 inputdata = 'data/gameboards/Rushhour6x6_1.csv'
 # Load all the vehicles
 
@@ -17,9 +17,35 @@ board = Board(inputdata)
  
 
 # print(moves)
+def dfs_states(board):
+    children = []
+    for car in board.vehicles:
+        for distance in range(-board.size + 1, board.size):
+            if distance != 0:
+                current_board = copy.deepcopy(board)
+                if current_board.move_car(car, distance):
+                    children.append(current_board)
 
-breath_algorithm(board)
+    return children
 
+def dfs_search(start_state):
+    queue = [[start_state]]
+    seen_states = set()
+    print("hello")
+    while queue:
+        path = queue.pop()
+        path[-1].print()
+        time.sleep(1)
+        if path[-1].won_game():
+            return True
+
+        for next_state in dfs_states(board):
+            print(next_state.print())
+            if next_state not in seen_states:
+                seen_states.add(next_state)
+                queue.append(path + [next_state])
+
+dfs_search(board)
 # board.move_car(board.vehicles[0], -1)
 # board.print()
 # board.move_car(board.vehicles[0], -1)
