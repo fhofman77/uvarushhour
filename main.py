450d1150d3089698ed1a5c11b1c1e1c7d786fb25
Frank Hofman <frank-hofman77@live.nl>
2021-06-15 12:47:22 +0200

    added output files

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from code.algorithms.breath_first import breath_algorithm
 from code.visualisation.visualise import print_board, create_csv
 from code.classes.objects import Board, moves, Car
 import copy, time
-inputdata = 'data/gameboards/Rushhour6x6_2.csv'
+inputdata = 'data/gameboards/Rushhour6x6_1.csv'
 # Load all the vehicles
 
 board = Board(inputdata)
 
 moves = breath_algorithm(board)
 algorithm = 'breath'
-board = 'Rushhour6x6_2'
+board = 'Rushhour6x6_1'
 
 
 create_csv(moves, algorithm, board)
