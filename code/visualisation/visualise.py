b3ac9add2a5183b77d7e2cc7a7c6b0a2b04cac26
Frank Hofman <frank-hofman77@live.nl>
2021-06-15 12:42:27 +0200

    submitting assignment

--- code/visualisation/visualise.py
+++ code/visualisation/visualise.py
@@ -1,8 +1,7 @@
+
 import csv
 
 """ versie waar car word meegegeven op de eerste plek """
-
-
 def print_board(occupied_row_col, board_size):
     for row in range(board_size):
         for col in range(board_size):
@@ -11,15 +10,15 @@ def print_board(occupied_row_col, board_size):
                 if car[1] == row+1 and car[2] == col+1:
                     print(car[0], end=' ')
                     not_printed = False
-
+            
             if not_printed:
                 print('# ', end='')
                 not_printed = False
         print('')
+            
 
 def create_csv(moves, algorithm, board):
     """Creates a CSV file from all the moves that have been added to a dict"""
     with open(f'data/output/output: {algorithm}, board: {board}.csv', 'w') as csv_file:
         writer = csv.writer(csv_file)
-        for key, value in moves.items():
-            writer.writerow([key, value])
+        writer.writerows(moves)
