159ecf32850b3b4be2651e539515ca2af6f0c752
Felix-Blom <70511386+Felix-Blom@users.noreply.github.com>
2021-06-19 10:09:58 +0200

    depth algorithm working

--- code/algorithms/breath_first.py
+++ code/algorithms/breath_first.py
@@ -1,5 +1,7 @@
 import queue
 import copy
+import time
+
 
 def breath_algorithm(board):
     def child_states(board):
@@ -37,10 +39,9 @@ def breath_algorithm(board):
                     visited.append(child[0].occupied_row_col)
                     all_states.append(child)
 
- 
         print('Could not find solution')
         return
-    
+
     solution = []
     all_states = solver(board)
     last_state = all_states.pop(-1)
@@ -56,8 +57,69 @@ def breath_algorithm(board):
         for state in all_states:
             if last_state[1] == state[0]:
                 last_state = state
-                
+
+
 def depth_algorithm(board):
+    start = time.time()
+
+    def child_states(board):
+        children = []
+        for car in board.vehicles:
+            for distance in range(-board.size + 1, board.size):
+                if distance != 0:
+                    child_board = copy.deepcopy(board)
+                    if child_board.move_car(car, distance):
+                        route = [child_board, board, car.car, distance]
+                        children.append(route)
+
+        return children
+
+    def solver(board):
+        que = queue.LifoQueue()
+        visited = []
+        que.put([board, board])
+        all_states = []
+
+        while not que.empty():
+            temp = que.get()
+            # print(que.qsize())
+            if que.qsize() != 1:
+                next_board = temp[-1]
+            next_board = temp
+            children = child_states(next_board[0])
+            for child in children:
+                if child[0].won_game():
+                    print('won')
+                    all_states.append(child)
+                    return all_states
+                if child[0].occupied_row_col not in visited:
+                    que.put(child)
+                    visited.append(child[0].occupied_row_col)
+                    all_states.append(child)
+
+        print('Could not find solution')
+        return
+
+    solution = []
+    all_states = solver(board)
+    last_state = all_states.pop(-1)
+    loop = True
+    # while last_state[1].occupied_row_col != board.occupied_row_col:
+    while loop:
+        solution.append([last_state[2], last_state[3]])
+        if last_state[1].occupied_row_col == board.occupied_row_col:
+            solution.reverse()
+            solution.insert(0, ['car', 'move'])
+            print(solution)
+            end = time.time()
+            print(end - start)
+            return(solution)
+        for state in all_states:
+            if last_state[1] == state[0]:
+                last_state = state
+
+
+"""def depth_algorithm(board):
     def child_states(board):
         children = []
         for car in board.vehicles:
@@ -128,3 +190,4 @@ def dfs_algorithm(board):
     
 
 
+"""
