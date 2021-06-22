9680663f6e9cc1a6d6df4e79d661a3a7d3909c01
Frank Hofman <frank-hofman77@live.nl>
2021-06-22 16:45:40 +0200

    data cleaned

--- code/algorithms/breadth_depth.py
+++ code/algorithms/breadth_depth.py
@@ -1,14 +1,15 @@
 import queue
 import copy
 
-"""
-A function where you can to apply a breadth of depth first algorithm
-"""
+
 def BFS_DFS(board, search_method):
     """
-    Creates the child states for a given board_state.
+    A function where you can to apply a breadth of depth first algorithm
     """
     def child_states(board):
+        """
+        Creates the child states for a given board_state.
+        """
         children = []
         # move every car all possibble places in the board
         for car in board.vehicles:
@@ -22,16 +23,16 @@ def BFS_DFS(board, search_method):
 
         return children
     
-    """
-    function that tries to solve the board in a breadth or depth first search algorithm
-    """
     def solver(board, search_method):
+        """
+        A function that tries to solve the board in a breadth or depth first search algorithm
+        """
         # check what algorithm is chosen to select the right que type
         if search_method.lower() == 'depth' or search_method.lower() == "d":
-            """If the user specifies depth first search method, usef LifoQueue"""
+            # If the user specifies depth first search method, usef LifoQueue
             que = queue.LifoQueue()
         elif search_method.lower() == 'breadth' or search_method.lower() == "b":
-            """Otherwise use breadthfirst search"""
+            # Otherwise use breadthfirst search
             que = queue.Queue()
         else:
             print('Enter a correct search method using breadth, depth, b or d')
@@ -76,61 +77,3 @@ def BFS_DFS(board, search_method):
         for state in all_states:
             if last_state[1] == state[0]:
                 last_state = state
-
-"""
-# test of itterative deepening
-def iterative_deepening(board):
-    def child_states(board):
-        children = []
-        for car in board.vehicles:
-            for distance in range(-board.size + 1, board.size):
-                if distance != 0:
-                    child_board = copy.deepcopy(board)
-                    if child_board.move_car(car, distance):
-                        route = [child_board, board, car.car, distance]
-                        children.append(route)
-
-        return children
-
-    def solver(board):
-        que = queue.LifoQueue()
-        visited = []
-        que.put([board, board])
-        all_states = []
-        counter = 0
-
-        while not que.empty() and counter < 300:
-            next_board = que.get()
-            children = child_states(next_board[0])
-            counter+=1
-            for child in children:
-                if child[0].won_game():
-                    print('won')
-                    all_states.append(child)
-                    return all_states
-                if child[0].occupied_row_col not in visited:
-                    que.put(child)
-                    visited.append(child[0].occupied_row_col)
-                    all_states.append(child)
-
-        print('Could not find solution')
-        return
-
-    solution = []
-    all_states = solver(board)
-    last_state = all_states.pop(-1)
-    loop = True
-    # while last_state[1].occupied_row_col != board.occupied_row_col:
-    while loop:
-        solution.append([last_state[2], last_state[3]])
-        if last_state[1].occupied_row_col == board.occupied_row_col:
-            solution.reverse()
-            solution.insert(0, ['car', 'move'])
-            print(solution)
-            return(solution)
-        for state in all_states:
-            if last_state[1] == state[0]:
-                last_state = state
-
-    solver(board)
-"""
--- code/classes/objects.py
+++ code/classes/objects.py
@@ -3,10 +3,10 @@ import re
 from code.visualisation.visualise import print_board
 import numpy as np
 
-"""
-Loads the board from the csv file
-"""
 def initialize_cars(csv_input):
+    """
+    Loads the board from the csv file
+    """
     # read and store vehicle information from file in correct order
     vehicles = []
     with open(csv_input, newline='') as gamefile:
@@ -31,11 +31,6 @@ def initialize_cars(csv_input):
                 occupied_row_col.append([car.car, x, car.col])
 
     return vehicles, occupied_row_col
-
-
-# moves = {
-#     'car': 'move'
-# }
 
 
 def get_board_size(inputdata):
@@ -70,17 +65,17 @@ class Board():
                 if self.valid_vertical_move(car, item[1], item[1]+distance) == False:
                     return False
 
+        # Making a move based on the give distance and board
         for item in self.occupied_row_col:
             if item[0] == car.car and car.orientation == "H":
                 item[2] = item[2] + distance
-                # moves[car.car] = distance
                 move_made += 1
 
             if item[0] == car.car and car.orientation == "V":
                 item[1] = item[1] + distance
-                # moves[car.car] = distance
                 move_made += 1
-
+        
+        # check whether the move is made correctly
         if move_made > 0:
             return True
         else:
@@ -90,6 +85,9 @@ class Board():
         return print_board(self.occupied_row_col, self.size)
 
     def valid_vertical_move(self, car, startpoint, endpoint):
+        """
+        checks if a vertical move is possible
+        """
         if endpoint <= 0 or endpoint > self.size:
             return False
         for item in self.occupied_row_col:
@@ -102,10 +100,13 @@ class Board():
             return True
 
     def valid_horizontal_move(self, car, startpoint, endpoint):
+        """
+        checks if a horizontal move is possible
+        """
         if endpoint <= 0 or endpoint > self.size:
             return False
 
-        """Change to car instead of car.car"""
+        # Change to car instead of car.car
         for item in self.occupied_row_col:
             # If the car from the board is in the same row
             if item[1] == car.row:
@@ -115,24 +116,13 @@ class Board():
 
         return True
 
-    def current_board(self):
-        """Add current board (after moves made)"""
-        return self.occupied_row_col
-        pass
-
     def won_game(self):
-            """If end_board == current_board return true"""
-            escape_car = self.occupied_row_col[-1]
-            if escape_car[2] == 6:
-                return True
-            else:
-                return False 
-
-
-    def past_board(self):
-        """if the current board == a past board after doing moves, it cant be the fastest so keep track of the past boards"""
-        pass
-
+        """If end_board == current_board return true"""
+        escape_car = self.occupied_row_col[-1]
+        if escape_car[2] == 6:
+            return True
+        else:
+            return False 
 
 class Car():
     def __init__(self, car, orientation, col, row, length):
